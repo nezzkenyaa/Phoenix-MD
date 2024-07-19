34b13a4c9f1eea66d8071d5984c17f1566592617
nezz4 <mogambisomo+1@gmail.com>
2024-07-19 19:52:03 +0300

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -73,7 +73,7 @@ Description : ${i.desc}\`\`\``
 
       menu += ``;
       return await message.client.sendMessage(message.jid, {
-        image: { url: `https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg` },
+        image: { url: `https://telegra.ph/file/f2506d3b5b6cb002ed431.jpg` },
         caption: tiny(menu),
         footer: tiny(
           `Phoenix-MD\nVersion : ${require("../package.json").version}`
